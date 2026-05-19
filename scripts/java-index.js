5b6ed47a9d2ae364390b3a734be54a371385350e
Erik Russell <erik.z.russell@gmail.com>
2026-05-18 20:58:19 -0700

    nav component for all pages

---
+++ scripts/java-index.js
@@ -0,0 +1,15 @@
+fetch("components/home-nav.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("home-nav-component").innerHTML = data;
+
+    //creating variables to hold the element reference
+    // make a reference for the element
+    const hamburger = document.getElementById("hamburger");
+    const navList = document.getElementById("nav-list");
+
+    hamburger.addEventListener("click", () => {
+        navList.classList.toggle("active");
+    });
+})
+.catch(error => console.error("Nav didn't load", error));
